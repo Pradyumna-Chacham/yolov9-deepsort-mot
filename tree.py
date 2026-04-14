ffbbd2ffc9747cc66ebba79f73ae9bd0baa6d6a2
Pradyumna-Chacham <94459410+Pradyumna-Chacham@users.noreply.github.com>
2026-04-14 15:34:48 -0400

    Delete tree.py

--- tree.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk('.'):
-    level = root.count(os.sep)
-    print('  ' * level + os.path.basename(root) + '/')
-    for f in files:
-        print('  ' * (level + 1) + f)
